56d777d211e13a4a4e0496b6fc57a4f1e0b10624
mudzso <mudzso@gmail.com>
2016-12-30 14:24:01 +0100

    solutions

---
+++ python-waste-recycling-in-an-oop-way-mudzso-master/dustbin.py
@@ -0,0 +1,40 @@
+from garbage import Garbage
+from paper_garbage import PaperGarbage
+from plastic_garbage import PlasticGarbage
+from dustbin_content_error import DustbinContentError
+
+
+class Dustbin:
+    
+    def __init__(self,color):
+        self.color = color
+        self.paper_content = []
+        self.plastic_content = []
+        self.house_waste_content = []
+
+    def throw_out_garbage(self,garbage):
+        if isinstance(garbage, PaperGarbage):
+
+            if garbage.is_squeezed:
+                self.paper_content.append(garbage)
+            else:
+                raise DustbinContentError
+
+        elif isinstance(garbage, PlasticGarbage):
+
+            if garbage.is_clean:
+                self.plastic_content.append(garbage)
+            else:
+                raise DustbinContentError
+
+        elif isinstance(garbage, Garbage):
+
+            self.house_waste_content.append(garbage)
+
+        else:
+            raise DustbinContentError
+
+    def empty_contents(self):
+        del self.house_waste_content[:]
+        del self.paper_content[:]
+        del self.plastic_content[:]
